8c77fa0201fb538baba11cfb78e1b6516f0168ae
Carlos Eduardo - Casa <eduardo201184@hotmail.com>
2024-05-27 22:25:33 -0300

    iniciando a calculadora de multa versao 2, interface ja pronta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculadoraContratosComponent } from './components/calculadora-contratos/calculadora-contratos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalculadoraContratosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/calculadora-contratos/calculadora-contratos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calculadora-contratos',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculadora-contratos.component.html',
+  styleUrl: './calculadora-contratos.component.css'
+})
+export class CalculadoraContratosComponent {
+
+}
