8aad6cfcc6c74dc0b3efabce1cae825f8b2ece4e
Carlos Eduardo - Trabalho <eduardo201184@hotmail.com>
2024-05-31 18:02:41 -0300

    feature: add dialog no feedback, usando o signal para atualizar os valores

---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog.component.html',
+  styleUrl: './dialog.component.css'
+})
+export class DialogComponent {
+
+}
--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { Component, ElementRef, QueryList, Renderer2, ViewChildren, signal } from '@angular/core';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
   selector: 'feedback',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DialogComponent],
   templateUrl: './feedback.component.html',
   styleUrl: './feedback.component.css',
 })
 export class FeedbackComponent {
+
+  isModalOpen = signal(false)
+
   @ViewChildren('starIcon') starIcons!: QueryList<ElementRef>;
   private globalClickListener: (() => void) | undefined;
 
@@ -39,5 +43,14 @@ export class FeedbackComponent {
     this.starIcons.forEach(star => {
       this.renderer.removeClass(star.nativeElement, 'ativo');
     });
+  }
+
+
+  modalOpen(){
+    this.isModalOpen.set(true)
+    console.log(this.isModalOpen)
+  }
+  toggleModal(){
+    this.isModalOpen.set(false)
   }
 }
